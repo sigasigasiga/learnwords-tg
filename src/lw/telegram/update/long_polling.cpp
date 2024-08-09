2c65981fac22becfac602cb04bd04140dd1c44f0
Egor Bychin <e.siga@proton.me>
2024-08-10 00:57:13 +0400

    implement long polling with `experimental::coro`

--- src/lw/telegram/update/long_polling.cpp
+++ src/lw/telegram/update/long_polling.cpp
@@ -2,10 +2,46 @@
 
 namespace lw::telegram::update {
 
-// interface
-boost::signals2::scoped_connection long_polling::connect(slot_t slot)
+boost::asio::experimental::coro<update_result, update_result> long_polling(
+    connection &conn,
+    std::chrono::seconds timeout,
+    int limit,
+    util::flag_set<allowed_updates> allowed
+)
 {
-    return signal_.connect(std::move(slot));
+    std::optional<std::int64_t> offset;
+
+    for(;;) {
+        boost::json::object params{
+            {"limit",   limit          },
+            {"timeout", timeout.count()},
+        };
+
+        if(allowed) {
+            params.emplace("allowed_updates", make_allowed_updates(allowed));
+        }
+
+        if(offset) {
+            params.emplace("offset", *offset);
+        }
+
+        auto updates_result =
+            co_await conn.async_request("getUpdates", std::move(params), boost::asio::deferred);
+
+        if(updates_result) {
+            auto &updates = updates_result->as_array();
+            if(!updates.empty()) {
+                offset = updates.back().at("update_id").as_int64() + 1;
+            }
+
+            for(auto &upd : updates) {
+                co_yield std::move(upd).as_object();
+                // TODO: discard the update somehow
+            }
+        } else {
+            co_yield std::unexpected{std::move(updates_result).error()};
+        }
+    }
 }
 
 } // namespace lw::telegram::update
