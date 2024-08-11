32722281bb03727d120abbc05040b6786232ce69
Egor Bychin <e.siga@proton.me>
2024-08-12 02:56:02 +0400

    i figured it out

--- src/lw/database/prepare.hpp
+++ src/lw/database/prepare.hpp
@@ -11,16 +11,11 @@ template<typename MysqlConnection, typename CompletionToken>
 requires boost::asio::completion_token_for<CompletionToken, async_prepare_completion_t>
 decltype(auto) async_prepare(MysqlConnection &conn, CompletionToken &&token)
 {
-    // honestly, i don't get how a coroutine may return `void`, but:
-    // 1. it works
-    // 2. asio documentation states that it should be `void`?
     auto impl = [](auto state, MysqlConnection &conn) -> void {
         boost::system::error_code ret;
         try {
             boost::mysql::results _;
 
-            // i don't know why we need to use `asio::deferred` here instead of
-            // `asio::use_awaitable`
             co_await conn.async_execute(query::create_db, _, boost::asio::deferred);
             co_await conn.async_execute(query::use_db, _, boost::asio::deferred);
             co_await conn.async_execute(query::create_sentences, _, boost::asio::deferred);
